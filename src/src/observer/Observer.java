2a0f7dedc57a24a69585d2b4cc30f1e5bb3fe77a
WeverttonSouza1 <contaparatestesws@gmail.com>
2025-11-29 17:35:02 -0300

    Delete src/src directory

--- src/src/observer/Observer.java
+++
@@ -1,5 +0,0 @@
-package observer;
-
-public interface Observer { // Implementada em cliente
-    void update(NotificacaoProduto notificacao);
-}
--- src/src/user/Administrador.java
+++
@@ -1,5 +0,0 @@
-package user;
-
-public class Administrador extends Usuario {
-    // para caso precise adicionar ações especificas para o administrador
-}
